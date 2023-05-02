98b821567f8f6a39d79959eff1de9fe804437204
ihorkryvoshlyk <ihorkryvoshlyk@gmail.com>
2023-05-02 18:24:14 +0300

    test api

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -87,6 +87,7 @@ export default function Checkout() {
         JSON.stringify({
           ...params,
           reference,
+          account: publicKey.toString(),
         })
       )
 
@@ -95,7 +96,6 @@ export default function Checkout() {
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify(body),
       })
     } else {
       const searchParams = new URLSearchParams()
